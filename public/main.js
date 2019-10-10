60ed4dcfcb0cef353a4361475f5c5e14f23f9244
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-10-11 01:28:46 +0530

    Add models

--- public/main.js
+++ public/main.js
@@ -1,5 +1,16 @@
 const video = document.getElementById('live_preview');
 
+Promise.all([
+	faceapi.nets.ageGenderNet.loadFromUri('/models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+	faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models'),
+	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+	faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+	faceapi.nets.mtcnn.loadFromUri('/models')
+]).then(startVideo);
+
 function startVideo() {
 	navigator.getUserMedia(
 		{ video: {} },
@@ -8,4 +19,6 @@ function startVideo() {
 	);
 }
 
-startVideo();
+video.addEventListener('play', () => {
+	console.info('Playing');
+});
