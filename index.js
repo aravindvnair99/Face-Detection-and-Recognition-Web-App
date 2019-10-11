8d5426f755fbfc05720c1e1e30cb7da42516d266
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-10-11 12:45:06 +0530

    Print IP address

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
 app.set('views', path);
 app.set('view engine', 'ejs');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function(ifname) {
+	var alias = 0;
+
+	ifaces[ifname].forEach(function(iface) {
+		if ('IPv4' !== iface.family || iface.internal !== false) {
+			// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+			return;
+		}
+
+		if (alias >= 1) {
+			// this single interface has multiple ipv4 addresses
+			console.log(ifname + ':' + alias, iface.address);
+		} else {
+			// this interface has only one ipv4 adress
+			console.log(ifname, iface.address);
+		}
+		++alias;
+	});
+});
 app.listen(app.get('port'), function() {
 	console.log('App is running on port', app.get('port'));
 });
